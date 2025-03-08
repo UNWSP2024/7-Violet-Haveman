d2cb0b3bafa997be194e544527331f77b43a83c3
Violet-Haveman <vkhaveman@students.unwsp.edu>
2025-03-07 20:12:19 -0600

    Week 7 Programs

--- program_3.py
+++ program_3.py
@@ -3,22 +3,41 @@ def main():
     # Have the user input (using a loop) various information that contains three pieces of data: 
     # year, name of state, and population.  
     # Store all of this information in a list of lists.  For example it might be stored like this:
-    
     # [[2010, "Maine", 1987435], [2010,"Minnesota",6873202], [2011, "Iowa", 3421988]]
     all_entered_values = []
-
-    # Now have the user enter a year. 
-    
+    year_to_sum = []
+    population = 0
+    pop_check = True
+    while pop_check:
+        year = str(input("Year: "))
+        state = str(input("State: "))
+        pop = int(input("Population: "))
+        small = [year, state, pop]
+        all_entered_values.append(small)
+        check = str(input("Add another? (Y/N): "))
+        if check == "Y":
+            continue
+        if check == "N":
+            break
+        else:
+           break
+    sum_population_for_year(all_entered_values, year_to_sum)
+    # Now have the user enter a year.
     # The program will add the populations from all states in the list of list for that year only.
     # Pass the list and year to the sum_population_for_year
-
 def sum_population_for_year(all_entered_values, year_to_sum):
-    # Loop through and sum the populations for the appropriate year. 
-    # e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
-    # or 3,421,988 if they enterd 2011 for the year to sum.
-
+            # Loop through and sum the populations for the appropriate year.
+        # e.g. for the list on line 7 the total would be 8,860,637 if the user enterd 2010 for the year to sum,
+        # or 3,421,988 if they enterd 2011 for the year to sum.
+    yr = input("Enter a year:")
     # print the totalled population
-
+    for year in all_entered_values:
+        if year == yr:
+            year_to_sum.append(all_entered_values)
+            tot = sum(year_to_sum)
+            print("Total Population from", yr, "=", tot)
+        else:
+            return all_entered_values
 
 # Call the main function.
 if __name__ == '__main__':
--- program_4.py
+++ program_4.py
@@ -7,4 +7,24 @@
 # The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
 # Also include exception handling to deal with faulty input.
 # The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2)
+def distance_problem(x1, x2, y1, y2, z1, z2):
+    dist1 = (x1, y1, y2)
+    dist2 = (x2, y2, z2)
+    distance = ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2)
+    print ("First Coordinates: ", dist1)
+    print("Second Coordinates:", dist2)
+    print ("Distance:", distance)
+def main():
+   try:
+       cor1 = int(input("X1 = "))
+       cor2 = int(input("X2 = "))
+       cor3 = int(input("Y1 = "))
+       cor4 = int(input("Y2 = "))
+       cor5 = int(input("Z1 = "))
+       cor6 = int(input("Z2 = "))
+       distance_problem(cor1, cor2, cor3, cor4, cor5, cor6)
+   except:
+       print ("Error: Try again")
+
+main()
